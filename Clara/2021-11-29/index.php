fb26ce2f4774c109b21a1e5f5442699424f9318a
Provavelmente o Caio <caiofecardoso@hotmail.com>
2022-04-25 15:58:33 -0300

    Remoção de pasta de ajuda a colega

--- Clara/2021-11-29/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-# include / incluse_once= ignora a segunda e bota só a primeira 
-require_once "includes/template.php"; #em caso de erro para completamente, nada funciona a baixo, é o melhor
-
